6b6a0ff01164e77bb222c036edaa0f43f84b9054
Erum Sikander <mrsikanderghouri@gmail.com>
2024-07-20 12:41:12 +0500

    done

---
+++ new.js
@@ -0,0 +1,17 @@
+"use strict";
+//objects
+Object.defineProperty(exports, "__esModule", { value: true });
+let teacher1 = {
+    name: "Erum Sikander",
+    age: 26,
+    city: "Pakistan",
+};
+console.log(teacher1);
+// dot notation:
+console.log(teacher1.name);
+console.log(teacher1.age);
+console.log(teacher1.city);
+// square notation: 
+console.log(teacher1["name"]);
+console.log(teacher1["age"]);
+console.log(teacher1["city"]);
---
+++ new.ts
@@ -0,0 +1,21 @@
+//objects
+
+let teacher1 : {
+    name : string,
+    age : number,
+    city : string,
+}
+= {
+    name : "Erum Sikander",
+    age : 26,
+    city : "Pakistan",
+}
+console.log(teacher1);
+// dot notation:
+console.log(teacher1.name);
+console.log(teacher1.age);
+console.log(teacher1.city);
+// square notation: 
+console.log(teacher1["name"]);
+console.log(teacher1["age"]);
+console.log(teacher1["city"]);
